0d32af30359bc61858ccf3e571748478b994a16b
Greg Fly <hrabiavulpes@gmail.com>
2022-11-19 08:47:00 +0100

    Initial commit

---
+++ src/main/java/org/example/controllers/SimpleController.java
@@ -0,0 +1,18 @@
+package org.example.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+    @GetMapping("/hello")
+    String hello() {
+        return "hello world!";
+    }
+
+    @GetMapping("/hello/{who}")
+    String helloYou(@PathVariable String who) {
+        return "hello " + who + "!";
+    }
+}
