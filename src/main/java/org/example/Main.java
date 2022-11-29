99140914511c921aad29cca951d7740e75cd4d88
Greg Fly <hrabiavulpes@gmail.com>
2022-11-29 09:47:40 +0100

    Some comments and name changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Run this from maven - mvn clean spring-boot:run
+ */
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
